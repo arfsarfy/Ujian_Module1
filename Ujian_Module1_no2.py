6f6e6bdfd5effb1b433a15193da680d325415de2
Arfyan <r.arfyan@gmail.com>
2018-11-28 15:16:40 +0700

    small addition

--- Ujian_Module1_no2.py
+++ Ujian_Module1_no2.py
@@ -2,14 +2,14 @@ def tickets(ppl=list):
     change = 0
     for item in ppl:
         if item-25 > change:
-            print('No')
+            print('No. She cannot')
             return False
         elif item > 25:
             change -= item - 25    
         change += 25
-    print('Yes')
+    print('Yes. It is possible')
     return True
 
 arr =[25,25,50,50,100]
-
+print(arr)
 tickets(arr)
